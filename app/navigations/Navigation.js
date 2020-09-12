7381b15ddbb4eb5bc08b25e81e9c44c3b8840544
julian1026 <33234916+julian1026@users.noreply.github.com>
2020-09-12 13:47:42 -0500

    add iconos a la barra de navegacion

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Icon} from 'react-native-elements'
 //trayendo los screen 
 
 import AccountStack from './AccountStack';
@@ -15,46 +16,46 @@ export default function Navigation(){
     return (
         <NavigationContainer>
             <Tab.Navigator
-            // initialRouteName='restaurants' // app inicie en el boton restaurants
-            // tabBarOptions={{   // se controla los colores del tabBar
-            //     inactiveTintColor:'#070707',//color general tabOptin
-            //     activeTintColor:'#34CD0E',//color especifico
-            // }}
+                initialRouteName='ubication' // app inicie en el boton restaurants
+                tabBarOptions={{   // se controla los colores del tabBar
+                    inactiveTintColor:'#070707',//color general tabOptin
+                    activeTintColor:'#34CD0E',//color especifico
+                }}
 
-            // // en esta parte cargo la funcion que me permitira cargar iconos
-            // screenOptions={({route})=>({
-            //     tabBarIcon:({color})=>screenOptions(route, color),
-            // })}
+                // en esta parte cargo la funcion que me permitira cargar iconos
+                screenOptions={({route})=>({
+                    tabBarIcon:({color})=>screenOptions(route, color),
+                })}
             >
                 
                 <Tab.Screen
-                name='ubication'
-                component={UbicationStack}
-                options={{title:"Ubicacion"}}
+                    name='ubication'
+                    component={UbicationStack}
+                    options={{title:"Ubicacion"}}
                  />
 
                  <Tab.Screen 
-                 name='favorites'
-                 component={FavoriteStack}
-                 options={{title:"Favoritos"}}
+                    name='favorites'
+                    component={FavoriteStack}
+                    options={{title:"Favoritos"}}
                  />
 
                  <Tab.Screen
-                 name='parkings'
-                 component={ParkingStack}
-                 options={{title:"Parqueaderos"}}
+                    name='parkings'
+                    component={ParkingStack}
+                    options={{title:"Parqueaderos"}}
                   />
 
                 <Tab.Screen
-                name='top-parkings'
-                component={TopParkingStack}
-                options={{title:"Top-5"}}
+                    name='top-parkings'
+                    component={TopParkingStack}
+                    options={{title:"Top-5"}}
                  />
 
                 <Tab.Screen
-                name='accounts'
-                component={AccountStack}
-                options={{title:"Cuenta"}}
+                    name='accounts'
+                    component={AccountStack}
+                    options={{title:"Cuenta"}}
                 />
 
 
@@ -63,5 +64,38 @@ export default function Navigation(){
     );
     
 }
+
+//funcion donde cargo icon dependiendo del  tabBoton
+function screenOptions(route,color){
+    let iconName;
+    switch (route.name) {
+        case 'ubication':
+            iconName='compass-outline'  
+            break;
+
+        case 'favorites':
+            iconName='heart-outline'
+            break;
+
+        case 'parkings':
+            iconName='magnify'
+            break;
+
+        case 'top-parkings':
+            iconName='star-outline'
+            break;
+
+        case 'accounts':
+            iconName='home-outline'
+            break;
+
+        default:
+            break;
+    }
+    return (
+        <Icon type='material-community' name={iconName} size={22} color={color} />
+    )
+}
+
 
 
