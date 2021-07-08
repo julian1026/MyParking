5a6a68d0ce84fbedbbfc99ffa586efcce8a06c15
julian1026 <juancaor1997@gmail.com>
2021-07-08 08:02:09 -0500

    funcional

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -7,53 +7,53 @@ import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import { assign } from "lodash";
 
-export default function InfoUSer(props){
-    const {
-        userInfo:{uid,photoURL,displayName,email},
-        toastRef,
-        setloading,
-        setLoadigText
-    }=props;
-    // console.log(props.userInfo)
-    const changeAvatar = async () => {
-      // console.log('julian');
-      const resultPermission = await Permissions.askAsync(
-        Permissions.CAMERA_ROLL 
-      );
-      console.log(resultPermission);
-      const resultPermissionCamera =resultPermission.permissions.cameraRoll.status;
-  
-      if (resultPermissionCamera === "denied") {
-        toastRef.current.show("Es necesario aceptar los permisos de la galeria");
+export default function InfoUSer(props) {
+  const {
+    userInfo: { uid, photoURL, displayName, email },
+    toastRef,
+    setloading,
+    setLoadigText
+  } = props;
+  // console.log(uid)
+  const changeAvatar = async () => {
+    // console.log('julian');
+    const resultPermission = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+    console.log(resultPermission);
+    const resultPermissionCamera = resultPermission.permissions.cameraRoll.status;
+
+    if (resultPermissionCamera === "denied") {
+      toastRef.current.show("Es necesario aceptar los permisos de la galeria");
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      // console.log(result);
+
+
+      if (result.cancelled) {
+        toastRef.current.show("Has cerrado la seleccion de imagenes");
       } else {
-        const result = await ImagePicker.launchImageLibraryAsync({
-          allowsEditing: true,
-          aspect: [4, 3],
-        });
-
-        console.log(result);
-
-
-        if (result.cancelled) {
-          toastRef.current.show("Has cerrado la seleccion de imagenes");
-        } else {
-          uploadImage(result.uri)
-            .then(() => {
-              console.log('image subida');
-
-              updatePhotoUrl();
-            })
-            .catch(() => {
-              console.log('image no subida');
-              // console.log(result.uri);
-              toastRef.current.show("Error al actualizar el avatar.");
-            });
-        }
+        uploadImage(result.uri)
+          .then(() => {
+            updatePhotoUrl();
+            // console.log('image subida');
+          })
+          .catch((error) => {
+            console.log('image no subida');
+            console.log(error);
+            setloading(false);
+            // console.log(result.uri);
+            toastRef.current.show("Error al actualizar el avatar.");
+          });
+      }
     }
   }
-  
+
   const uploadImage = async (uri) => {
-    // console.log(uri);
 
     setLoadigText("Actualizando Avatar");
     setloading(true);//mostrando mensaje mientras actualiza imagen
@@ -68,64 +68,79 @@ export default function InfoUSer(props){
   };
   //UTILIZO EL MISMO UID DEL USUARIO PARA TENER SOLO UN IDENTIFICA
   // Y PODER CAMBIAR LA IMAGEN
-  const updatePhotoUrl=()=>{
+  const updatePhotoUrl = () => {
     firebase
-    .storage()
-    .ref(`avatar/${uid}`)
-    .getDownloadURL()
-    .then(async(response) =>{
-      const update={
-        photoURL:response,
-      };
-      await firebase.auth().currentUser.updateProfile(update);//actualizo imagen
-      console.log("imagen actualizada");
-      setloading(false);
-    })
-    .catch(()=>{
-      toastRef.current.show("Error al actualizar el avatar.");
-    })  
+      .storage()
+      .ref(`avatar/${uid}`)
+      .getDownloadURL()
+      .then(async (response) => {
+        const update = {
+          photoURL: response,
+        };
+        await firebase.auth().currentUser.updateProfile(update);//actualizo imagen
+        // console.log("imagen actualizada");
+        updateUserPhoto(response);
+        setloading(false);
+      })
+      .catch(() => {
+        setloading(false);
+        toastRef.current.show("Error al actualizar el avatar.");
+      })
   };
 
+  //collecion user firestore Update photoURL
+  const updateUserPhoto = (photo) => {
+    const update = {
+      photoURL: photo
+    }
+    firebase.firestore().collection("users").doc(uid).update(update)
+      .then(() => {
+        console.log('ok');
+      }).catch((error) => {
+        console.log('collection users in firestore image does not update')
+      })
 
-    return(
-        <View style={styles.viewUserInfo} >
-            <Avatar
-                rounded
-                size="large"
-                showEditButton
-                
-                onEditPress={changeAvatar}
-                containerStyle={styles.userInfoAvatar}     
-                source={ 
-                    photoURL
-                     ?{uri:photoURL}
-                     :require('../../../assets/img/avatar-default.jpg')}
-             />
-             <View>
-                 <Text
-                  style={styles.displayName}>
-                      {displayName?displayName:'Anonimo'}
-                 </Text>
-                <Text>{email?email:'Social Login'}</Text>
-             </View>
-        </View>
-    )
+  }
+
+  return (
+    <View style={styles.viewUserInfo} >
+      <Avatar
+        rounded
+        size="large"
+        showEditButton
+
+        onEditPress={changeAvatar}
+        containerStyle={styles.userInfoAvatar}
+        source={
+          photoURL
+            ? { uri: photoURL }
+            : require('../../../assets/img/avatar-default.jpg')}
+      />
+      <View>
+        <Text
+          style={styles.displayName}>
+          {displayName ? displayName : 'Anonimo'}
+        </Text>
+        <Text>{email ? email : 'Social Login'}</Text>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    viewUserInfo: {
-      alignItems: "center",
-      justifyContent: "center",
-      flexDirection: "row",
-      backgroundColor: "#f2f2f2",
-      paddingTop: 30,
-      paddingBottom: 30,
-    },
-    userInfoAvatar: {
-      marginRight: 20,
-    },
-    displayName: {
-      fontWeight: "bold",
-      paddingBottom: 5,
-    },
-  });
+  viewUserInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f2f2f2",
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  userInfoAvatar: {
+    marginRight: 20,
+  },
+  displayName: {
+    fontWeight: "bold",
+    paddingBottom: 5,
+  },
+});
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,6 +1,6 @@
-import React, {useRef, useState, useEffect} from 'react'
-import { StyleSheet,View,Text} from 'react-native';
-import {Button} from 'react-native-elements';
+import React, { useRef, useState, useEffect } from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 import Toast from 'react-native-easy-toast';
 import * as firebase from 'firebase';
 
@@ -10,45 +10,46 @@ import InfoUser from '../../components/Account/InfoUser';
 import { set } from 'lodash';
 import AccountOptions from '../../components/Account/AccountOptions';
 
-export default function UserLogged(){
+export default function UserLogged() {
     const [userInfo, setUserInfo] = useState(null);
     const [loading, setloading] = useState(false);
-    const toastRef=useRef();
+    const toastRef = useRef();
     const [loadigText, setLoadigText] = useState('');
     const [reloadUseInfo, setReloadUseInfo] = useState(false);
 
 
     useEffect(() => {
-        (async()=>{
-            const user=await firebase.auth().currentUser;
+        (async () => {
+            const user = await firebase.auth().currentUser;
             setUserInfo(user);
         })()
         setReloadUseInfo(false);
-    },
-     [reloadUseInfo])
 
-    return(
+    },
+        [reloadUseInfo])
+
+    return (
         <View style={styles.viewUserInfo} >
             {userInfo && <InfoUser
-                 userInfo={userInfo}
-                  toastRef={toastRef}
-                  setloading={setloading}
-                  setLoadigText={setLoadigText} /> }
-            
+                userInfo={userInfo}
+                toastRef={toastRef}
+                setloading={setloading}
+                setLoadigText={setLoadigText} />}
+
             {/* <Text>AccountOptions...</Text> */}
             <AccountOptions
-            userInfo={userInfo}
-            toastRef={toastRef}
-            setReloadUseInfo={setReloadUseInfo}
+                userInfo={userInfo}
+                toastRef={toastRef}
+                setReloadUseInfo={setReloadUseInfo}
             />
 
 
-            <Button 
+            <Button
                 title='Cerrar Session'
                 buttonStyle={styles.btnCloseSession}
                 titleStyle={styles.btnCloseSessionText}
-                onPress={()=> firebase.auth().signOut()}
-             />
+                onPress={() => firebase.auth().signOut()}
+            />
             <Toast ref={toastRef} position='center' opacity={0.9} />
             <Loading text={loadigText} isVisible={loading} />
 
@@ -56,24 +57,24 @@ export default function UserLogged(){
     );
 }
 
-const styles=StyleSheet.create({
-    viewUserInfo:{
-        minHeight:'100%',
-        backgroundColor:'#f2f2f2'
-        
+const styles = StyleSheet.create({
+    viewUserInfo: {
+        minHeight: '100%',
+        backgroundColor: '#f2f2f2'
+
     },
-    btnCloseSession:{
-        marginTop:30,
-        borderRadius:0,
-        backgroundColor:'#fff',
+    btnCloseSession: {
+        marginTop: 30,
+        borderRadius: 0,
+        backgroundColor: '#fff',
         borderTopWidth: 1,
         borderTopColor: '#e3e3e3',
         borderBottomWidth: 1,
-        borderBottomColor:'#e3e3e3',
-        paddingTop:10,
-        paddingBottom:10,
+        borderBottomColor: '#e3e3e3',
+        paddingTop: 10,
+        paddingBottom: 10,
     },
-    btnCloseSessionText:{
-        color:'#00a680',
+    btnCloseSessionText: {
+        color: '#00a680',
     }
 })
